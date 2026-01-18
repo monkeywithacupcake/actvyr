ff165a7a98d0679c19f0e6d440b8a6b31f7e317d
jess <jessachandler@gmail.com>
2026-01-17 16:31:09 -0800

    avoids elev avg err

--- R/plots.R
+++ R/plots.R
@@ -115,12 +115,13 @@ clean_monthly_type_summary <- function(df, typecolors = NULL) {
   df$type <- factor(df$type,
                     levels=typecolors$type)
   tmp <- df %>%
+    mutate(elevation = elev) %>% # rename for mean
     group_by(month, type) %>%
     summarise(distance = sum(distance, na.rm = TRUE), # miles
               elev = sum(elev, na.rm = TRUE),         # ?
               time = sum(duration, na.rm = TRUE)/60,    # hours
               avgdist = mean(distance, na.rm = TRUE), #mi/run
-              avgelev = mean(elev, na.rm = TRUE), # ft/run
+              avgelev = mean(elevation, na.rm = TRUE), # ft/run
               avgpace = mean(pace, na.rm = TRUE), # min/mile
               count = n(),
               .groups = "drop")
